dae94d31317e725107ddff8ecbfbc712a9334a1a
Afef <afef.mokhtari@esprit.tn>
2016-08-09 12:29:20 +0200

    fix Mongo connection string223

--- config/db.js
+++ config/db.js
@@ -1,12 +1,8 @@
-/*
-*
-*MONGOLAB_URI = mongodb://heroku_f92qr4gs:kgqjkna91571lk6fpssdcsl7ho@ds145415.mlab.com:45415/heroku_f92qr4gs
-*
-*/
+/*MONGOLAB_URI = mongodb://heroku_f92qr4gs:kgqjkna91571lk6fpssdcsl7ho@ds145415.mlab.com:45415/heroku_f92qr4gs*/
 
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/roomie');
-mongoose.connect(process.env.MONGOLAB_URI, function(error){
+dblocal = 'mongodb://localhost/roomie';
+mongoose.connect(process.env.MONGOLAB_URI || dblocal, function(error){
 	if(error)
 		console.error(error);
 	else console.log('mongo connected');
