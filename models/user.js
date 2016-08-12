d1b9daf707b48d7a154814b7d0a70d96cdb10816
Afef <afef.mokhtari@esprit.tn>
2016-08-12 11:35:33 +0200

    conflit résolu

--- models/user.js
+++ models/user.js
@@ -2,11 +2,7 @@ var mongoose = require('../config/db');
 
 var UserSchema = mongoose.Schema({
     nom_prenom: String,
-<<<<<<< HEAD
-    assword: String,
-=======
     password: String,
->>>>>>> 9c521219d17c0651ae0bebaf0fc1b09237b5305e
     username: String,
     email: String,
     date_naissance: Date,
