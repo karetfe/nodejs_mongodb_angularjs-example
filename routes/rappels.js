60dac79110048bfde20baebeddd3054317a4232e
Afef <afef.mokhtari@esprit.tn>
2016-09-15 17:49:45 +0200

    users.js

--- routes/rappels.js
+++ routes/rappels.js
@@ -40,12 +40,12 @@ router.post('/', function(req, res, next) {
 
 /* update rappel by autoincrement modif {$inc: { modif: 1} }*/
 router.put('/:id', function(req, res, next) {
-    //var data = req.body;
-    //if(data.posts == undefined) data.posts = [];    
-    models.rappel.findByIdAndUpdate(req.params.id, {$inc: { modif: 1}}, {new: true}, function(err, c){
+    var data = req.body;
+    if(data.posts == undefined) data.posts = [];    
+    models.rappel.findByIdAndUpdate(req.params.id, {new: true}, function(err, c){
         if(err) res.json({error: err});
-        
-        res.json(c).send({modif: c.modif});
+        res.json(c);
+        //res.json(c).send({modif: c.modif});
   });
 });
 
