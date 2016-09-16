d2dfcd254bda40775a1f2d6c0192fe086c9493fe
Afef <afef.mokhtari@esprit.tn>
2016-09-16 14:15:49 +0200

    rappels.js

--- app.js
+++ app.js
@@ -54,42 +54,29 @@ app.post('/authenticate', function(req, res) {
      });   
   UserWithRobots.findOne({
     username: req.body.username
-  }, function(err, user,robot) {
-
+  }, function(err, user) {
     if (err) throw err;
-
     if (!user) {
       res.json({ success: false, message: 'Authentication failed. User not found :-)' });
     } else if (user) {
-
       // check if password matches
       if (user.password != req.body.password) {
         res.json({ success: false, message: 'Authentication failed. Wrong password.' });
       } else {
-
         // if user is found and password is right
         // create a token
         var token = jwt.sign(user, require('./config/jwt'), {
           //expiresInMinutes: 1440 // expires in 24 hours
-        });
-          
+        });          
         // return the information including token as JSON
-
         res.json({user,              
           success: true, 
           message: 'Authenticated: Enjoy your token!',
-          token: token,
-
-        });
-	
+          token: token
+        });	
       }
-
     }
-
-  }); 
-    
-    
-    
+  });     
 });
 
 app.use('/', require('./routes'));
